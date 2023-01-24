a1151266e3475a5a52b22c528171927b545bcfa4
Javier Hernández <jhernandez50@gmail.com>
2023-01-24 17:14:41 -0400

    Updates

---
+++ precache-manifest.ab30353517ba12a9f41feb02354e7f02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "44bc187ba512b9b3b5d5",
+    "url": "/todolist/static/css/main.42fabff4.chunk.css"
+  },
+  {
+    "revision": "44bc187ba512b9b3b5d5",
+    "url": "/todolist/static/js/main.44bc187b.chunk.js"
+  },
+  {
+    "revision": "7f8c4fcd7c0187552f17",
+    "url": "/todolist/static/js/1.7f8c4fcd.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "8c0b3f7cea1d891a63c1755b0833a13e",
+    "url": "/todolist/static/media/checkbox.8c0b3f7c.svg"
+  },
+  {
+    "revision": "9b8d46c07ebbc7e651133c437f87a194",
+    "url": "/todolist/static/media/trash.9b8d46c0.svg"
+  },
+  {
+    "revision": "42e7262c259516005c569ee98424c131",
+    "url": "/todolist/index.html"
+  }
+];
